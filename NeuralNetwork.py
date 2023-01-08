ed8689b620b6e21010274d423bdbb31e4b27280a
Steven-dR119 <ibvc119@gmail.com>
2023-01-08 01:23:46 +0100

    NN update

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,25 +1,54 @@
-"""
-Created on Mon Dec 12 11:15:10 2022
-
-@author: Steven
-"""
 import matplotlib.pyplot as plt
-import re
-import shutil
-import string
-import tensorflow as tf
-from tensorflow.keras import layers
-from tensorflow.keras import losses
-print(tf.__version__)
-import selfies as sf
 import numpy as np
-import os
 import pandas as pd
-import glob
-import sys
-from General_functions import ML_database ,simple_database, make_IAST_database_Wessel_version
-#Make the database of the molecule representation
+import seaborn as sns
+
+import tensorflow as tf
+
+from tensorflow import keras
+from tensorflow.keras import layers
+
+from General_functions import ML_database ,simple_database, make_IAST_database_Wessel_version  
+def plot_loss(history):
+    plt.figure()
+    plt.plot(history.history['loss'], label='loss')
+    plt.plot(history.history['val_loss'], label='val_loss')
+    plt.ylim([0,10])
+    plt.xlabel('Epochs')
+    plt.ylabel('Error [MPG]')
+    plt.legend()
+    plt.grid(True)
+
+def plot_horsepower(x,y):
+    plt.figure()
+    plt.scatter(train_features['Horsepower'], train_labels, label='Data')
+    plt.plot(x, y, color = 'k', label='Predictions')
+    plt.xlabel('Horsepower')
+    plt.ylabel('MPG')
+    plt.legend()
+
+def build_and_compile_model(norm):
+    model = keras.Sequential([
+        norm,
+        layers.Dense(64, activation = 'relu'),
+        layers.Dense(64, activation = 'relu'),
+        layers.Dense(1)
+    ])
+    
+    model.compile(loss='mean_absolute_error',
+                  optimizer = tf.keras.optimizers.Adam(0.001))
+    return model
+
+print(tf.__version__)
+
+
+#import database of molecules representation
 selfies_database = ML_database()
 easy_database = simple_database()
 
-x_data, y_data = make_IAST_database_Wessel_version(easy_database,3)
+#getting IAST combined with the molecule representation
+x_data, y_data = make_IAST_database_Wessel_version(easy_database, 3)
+
+
+
+train_dataset = dataset.sample(frac=0.8, random_state=0)
